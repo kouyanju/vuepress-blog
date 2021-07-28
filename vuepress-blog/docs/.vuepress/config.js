672851a86e26a244dcc61adf074f1715ea80037b
kouyanju <17865815392@163.com>
2021-07-28 15:44:04 +0800

    初建blog框架

---
+++ vuepress-blog/docs/.vuepress/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    title:"Glowing'blog",
+    description:"May everything you do for it have romantic results",
+    themeConfig:{
+        logo: '/assets/hero.png',//添加导航栏logo
+    }
+
+  }
